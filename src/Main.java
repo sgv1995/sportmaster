ed5088283b7c139d64f128a9a84de66c701cb4f0
Grisha <grisha5991@gmail.com>
2019-09-25 20:45:14 +0300

    fixed #2 Поменял тип данных на int

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите сумму прошлых покупок без учёта последней покупки: ");
-        double previousPurchases = in.nextDouble();
+        int previousPurchases = in.nextInt();
         System.out.println("Введите сумму последней покупки:");
-        double currentPurchase = in.nextDouble();
-        double allPurchase = previousPurchases + currentPurchase;
-        double difference = 1000 - allPurchase % 1000;
+        int currentPurchase = in.nextInt();
+        int allPurchase = previousPurchases + currentPurchase;
+        int difference = 1000 - allPurchase % 1000;
         if (difference < 200) {
-            DecimalFormat df = new DecimalFormat("#.00");
-            System.out.println("Для получения большего количеста бонусов рекомендуем докупить товар на сумму: " + (df.format(difference)));
+            System.out.println("Для получения большего количеста бонусов рекомендуем докупить товар на сумму: " + difference);
         }
         int totalAmount = 1000;
         if (allPurchase > 1 && allPurchase <= 15000) {
-            double bonus = Math.floor(allPurchase / totalAmount) * 50;
+            int bonus = allPurchase / totalAmount * 50;
             System.out.println("Вы получите бонусов: " + bonus);
         } else if (allPurchase >= 15001 && allPurchase <= 150000) {
-            double bonus = Math.floor(allPurchase / totalAmount) * 70;
+            int bonus = allPurchase / totalAmount * 70;
             System.out.println("Вы получите бонусов: " + bonus);
         } else if (allPurchase > 150000) {
-            double bonus = Math.floor(allPurchase / totalAmount) * 100;
+            int bonus = allPurchase / totalAmount * 100;
             System.out.println("Вы получите бонусов: " + bonus);
         }
 
